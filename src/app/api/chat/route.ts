5e763d530854ed1f387c9fb805212d0a9e732e6c
Abrar018 <abrar.rehan@ucalgary.ca>
2024-10-27 19:00:02 -0600

    got chat asking questions

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,28 +1,114 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import {
+    Message as VercelChatMessage,
+    StreamingTextResponse,
+    createStreamDataTransformer
+} from 'ai';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { HttpResponseOutputParser } from 'langchain/output_parsers';
 
-// Optional, but recommended: run on the edge runtime.
-// See https://vercel.com/docs/concepts/functions/edge-functions
-export const runtime = 'edge';
+import { JSONLoader } from "langchain/document_loaders/fs/json";
+import { RunnableSequence } from '@langchain/core/runnables'
+import { formatDocumentsAsString } from 'langchain/util/document';
+import { CharacterTextSplitter } from 'langchain/text_splitter';
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY!,
-});
+// const loader = new JSONLoader(
+//     "src/data/question.json",
+//     ["/state", "/code", "/nickname", "/website", "/admission_date", "/admission_number", "/capital_city", "/capital_url", "/population", "/population_rank", "/constitution_url", "/twitter_url"],
+// );
+
+export const dynamic = 'force-dynamic'
+
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+    return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `Ask the user the following questions one by one and store the answers and come up with a conclusion at the end of the conversation.
+
+E1	I am the life of the party.
+E2	I don't talk a lot.
+E3	I feel comfortable around people.
+E4	I keep in the background.
+E5	I start conversations.
+E6	I have little to say.
+E7	I talk to a lot of different people at parties.
+E8	I don't like to draw attention to myself.
+E9	I don't mind being the center of attention.
+E10	I am quiet around strangers.
+
+==============================
+Context: {context}
+==============================
+Current conversation: {chat_history}
+
+user: {question}
+assistant:`;
+
 
 export async function POST(req: Request) {
-    // Extract the `messages` from the body of the request
-    const { messages } = await req.json();
+    try {
+        // Extract the `messages` from the body of the request
+        const { messages } = await req.json();
 
-    // Request the OpenAI API for the response based on the prompt
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        stream: true,
-        messages: messages,
-    });
+        const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
 
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(response);
+        const currentMessageContent = messages[messages.length - 1].content;
 
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
+        // const docs = await loader.load();
+
+        // load a JSON object
+        const textSplitter = new CharacterTextSplitter();
+        const docs = await textSplitter.createDocuments([JSON.stringify({
+            "E1"	:"I am the life of the party.",
+            "E2"	:"I don't talk a lot.",
+            "E3"	:"I feel comfortable around people.",
+            "E4"	:"I keep in the background.",
+            "E5"	:"I start conversations.",
+            "E6"	:"I have little to say.",
+        })]);
+
+        const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+
+        const model = new ChatOpenAI({
+            apiKey: process.env.OPENAI_API_KEY!,
+            model: 'gpt-4o',
+            temperature: 0,
+            streaming: true,
+            verbose: true,
+        });
+
+        /**
+       * Chat models stream message chunks rather than bytes, so this
+       * output parser handles serialization and encoding.
+       */
+        const parser = new HttpResponseOutputParser();
+
+        const chain = RunnableSequence.from([
+            {
+                question: (input) => input.question,
+                chat_history: (input) => input.chat_history,
+                context: () => formatDocumentsAsString(docs),
+            },
+            prompt,
+            model,
+            parser,
+        ]);
+
+        // Convert the response into a friendly text-stream
+        const stream = await chain.stream({
+            chat_history: formattedPreviousMessages.join('\n'),
+            question: currentMessageContent,
+        });
+
+        // Respond with the stream
+        return new StreamingTextResponse(
+            stream.pipeThrough(createStreamDataTransformer()),
+        );
+    } catch (e: any) {
+        return Response.json({ error: e.message }, { status: e.status ?? 500 });
+    }
 }
